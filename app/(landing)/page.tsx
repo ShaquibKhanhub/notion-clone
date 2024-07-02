bff45dde6aeeaa6f8b7aeda1a3de1368298b4122
Shaquibkhanhub <shaquibkhanrdx34@gmail.com>
2024-07-02 20:04:48 +0530

    still working...

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const LandinPage = () => {
+  return (
+    <div className="min-h-full flex flex-col">
+      <Button>yo</Button>
+    </div>
+  );
+};
+
+export default LandinPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import LandinPage from "./(landing)/page";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-red-300 font-bold">hello notion clone</h1>
-      <Button variant="outline">Button</Button>
+     <LandinPage/>
     </>
   );
 }
